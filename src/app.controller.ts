4bee0c25ef0310326585652d70ceb03f9d0936e9
DoutorK <giovannylucas28@gmail.com>
2025-05-29 17:25:11 -0300

    refactor: add return types to getHealthStatus and getVersion methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { HealthStatus, AppInfo } from './interfaces/app.interfaces';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
     @Get()
-    getHealthStatus() {
+    getHealthStatus(): HealthStatus {
         return this.appService.getHealthStatus();
     }
 
     @Get('version')
-    getVersion() {
+    getVersion(): AppInfo {
         return this.appService.getAppInfo();
     }
 }
